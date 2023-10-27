f2943e1d63a6467f44fab3947aa4af925cf26866
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-10-27 11:34:17 -0400

    Add files via upload

---
+++ Model.java
@@ -0,0 +1,11 @@
+
+public class Model {
+	private StateType currentState;
+	
+	public StateType getCurrentState() {
+		return currentState;
+	}
+	public void setCurrentState(StateType currentState) {
+		this.currentState = currentState;
+	}
+}
---
+++ StreetOutsideHouse.java
@@ -0,0 +1,8 @@
+
+public class StreetOutsideHouse extends State{
+	private Model model;
+	
+	public StreetOutsideHouse(Model model) {
+		this.model = model;
+	}
+}
